5d0d5d5a619f952d33a0c5f64adf14a4b5f20c4b
Stanimir Zhekov <s.zhekov1718@gmail.com>
2023-05-29 18:03:48 +0300

    Add exercises up to 3.8

---
+++ part3/index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const app = express();
+
+const morgan = require("morgan");
+
+app.use(express.json());
+app.use(morgan("tiny"));
+
+morgan.token("body", (request) => JSON.stringify(request.body));
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  person = persons.filter((person) => person.id !== id);
+
+  if (!person) {
+    return response.status(404).json({
+      error: "person does not exist",
+    });
+  }
+
+  response.status(204).end();
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const person = persons.find((person) => person.id === id);
+
+  if (!person) {
+    return response.status(404).json({
+      error: "person does not exist",
+    });
+  }
+
+  response.json(person);
+});
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<div> Phonebook has info for ${persons.length} people. <p>${request} <p><div>`
+  );
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
