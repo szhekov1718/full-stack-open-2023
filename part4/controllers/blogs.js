22a9463477e61943ebaf74f2500de281b1af0eea
Stanimir Zhekov <s.zhekov1718@gmail.com>
2023-06-12 16:06:43 +0300

    Delete dummy test file

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,16 +1,51 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
-  response.json(blogs);
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
-blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.get("/:id", async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id);
+    if (blog) {
+      response.json(blog);
+    } else {
+      response.status(404).end();
+    }
+  } catch (exception) {
+    next(exception);
+  }
+});
 
-  await blog.save();
-  response.status(201).json(blog);
+blogsRouter.post("/", async (request, response, next) => {
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+  });
+
+  try {
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+blogsRouter.delete("/:id", async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = blogsRouter;
--- part4/models/blog.js
+++ part4/models/blog.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: String,
-  author: String,
+  author: {
+    type: String,
+    required: true,
+  },
   url: String,
   likes: Number,
 });
